b76e68864b3aee3522534e58083c0f4ebca778e3
ZRahm <rahmanova.zarina91@gmail.com>
2023-08-20 12:49:58 -0400

    test

--- force-app/main/default/lwc/techProEdComp/techProEdComp.js
+++ force-app/main/default/lwc/techProEdComp/techProEdComp.js
@@ -12,6 +12,9 @@ export default class TechProEdComp extends LightningElement {
     punchline;
     type;
 
+    word ="";
+    res;
+
     valChange(event){
         console.log("valchange methos is invoked");
         console.log(event);
@@ -36,7 +39,20 @@ callRest(){
      });
 }
 
+isPalindrome(event){
+    console.log(event);
+    console.log(event.target.value);
+this.word = event.target.value;
+}
     
+isSubmit(){
+    console.log('Submit function');
+    console.log(this.word);
 
+    let s1 = this.word.split(''); // convert string into array
+    let s2 = s1.reverse(); //reverse function
+    let rMessage = s2.join('');// convert array into string
+    this.res = rMessage;
+}
 
 }
