1e4190ae49611ce21d6040cd2b46127bccacbedf
ZRahm <rahmanova.zarina91@gmail.com>
2023-08-20 12:28:09 -0400

    update

--- force-app/main/default/lwc/techProEdComp/techProEdComp.js
+++ force-app/main/default/lwc/techProEdComp/techProEdComp.js
@@ -7,13 +7,34 @@ export default class TechProEdComp extends LightningElement {
     city = "Lynnfield";
     country="USA";
 
+    id;
+    setup;
+    punchline;
+    type;
+
     valChange(event){
         console.log("valchange methos is invoked");
         console.log(event);
         console.log(event.target.value);
         this.name = event.target.value;
     }
-
+callRest(){
+    console.log('callREST  method is invoked');
+    fetch("https://official-joke-api.appspot.com/random_joke", {method:"GET"})
+    .then( response => response.json()) 
+    .then(data => {
+        console.log(data); 
+        console.log(data.id); 
+        console.log(data.setup); 
+        console.log(data.punchline); 
+        console.log(data.type); 
+        this.id = data.id; 
+        this.setup =data.setup;
+        this.punchline =data.punchline;
+        this.type =data.type;
+        
+     });
+}
 
     
 
