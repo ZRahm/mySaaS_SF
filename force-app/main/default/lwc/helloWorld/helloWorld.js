fd32f5788fe899bf9b31a708240ab99a4a8f7d0e
ZRahm <rahmanova.zarina91@gmail.com>
2023-08-22 19:03:33 -0400

    new LWC

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,23 @@
 import { LightningElement } from 'lwc';
+
 export default class HelloWorld extends LightningElement {
-  greeting = 'World';
-  changeHandler(event) {
-    this.greeting = event.target.value;
-  }
+      // containers in JS are called properties. There are 2 types: Local and Public
+      //Local - Variables that are available only in this component
+      //Public Properties: vars that are available for external component as well. We will focus on this in chapter 5. component communication
+
+       name = "Salesforce Developer";
+       title = "Salesforce DEv with Certificate";
+       experience = 3; 
+       fruits = ["apple", "banana", "pear", "orange"];
+       location = {
+        city : "Boston",
+        state : "MA",
+        postalCode : "01234"
+       }
+
+
+
+
+
+
 }
